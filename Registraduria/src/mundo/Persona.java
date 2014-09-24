b2c2e37d957771c9238e40466a3bf1283c818102
Edwin <edwinb089@hotmail.com>
2014-09-24 17:11:32 -0500

    se añadio un atributo que representa la edad de un mansito XD

--- Registraduria/src/mundo/Persona.java
+++ Registraduria/src/mundo/Persona.java
@@ -6,10 +6,20 @@ public class Persona {
 
 	private String nombre;
 	private String id;
-	public Persona(String nombre, String id) {
+	private int edad;
+	
+	public Persona(String nombre, String id, int edad) {
 		super();
 		this.nombre = nombre;
 		this.id = id;
+	    this.edad = edad;
+	    
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
 	}
 	public String getNombre() {
 		return nombre;
