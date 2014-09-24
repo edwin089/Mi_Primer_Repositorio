fb4190d53e6bf3bedd08b6a36ac0a464126c973d
Edwin <edwinb089@hotmail.com>
2014-09-24 17:00:23 -0500

    Commit inicial

---
+++ Registraduria/src/mundo/Persona.java
@@ -0,0 +1,26 @@
+package mundo;
+
+public class Persona {
+
+	
+
+	private String nombre;
+	private String id;
+	public Persona(String nombre, String id) {
+		super();
+		this.nombre = nombre;
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+}
